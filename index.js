0a4c33a0419f7afc05b74b897fe0d4447590b3d9
jniac <joseph.merdrignac@gmail.com>
2017-10-19 00:57:43 +0200

    fixes + chalk

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const serveIndex = require('serve-index')
 const path = require('path')
 const fs = require('fs')
+const chalk = require('chalk')
 
 const args = process.argv.slice(2).reduce((acc, arg) => {
 
@@ -14,11 +15,13 @@ const args = process.argv.slice(2).reduce((acc, arg) => {
 }, {})
 
 let [port = 8000] = process.argv.slice(2).filter(v => /\d+/.test(v)).map(parseFloat)
-let [dir = __dirname] = process.argv.slice(2).filter(v => /\.|\//.test(v)).map(v => path.join(__dirname, v))
+let [dir = '.'] = process.argv.slice(2).filter(v => /\.|\//.test(v))
+
+dir = path.join(process.cwd(), dir)
 
 if (!fs.existsSync(dir)) {
 
-	console.log(`${dir} is not a valid folder`)
+	console.log(`${chalk.red(dir)} is not a valid folder`)
 	process.exit()
 
 }
@@ -26,15 +29,27 @@ if (!fs.existsSync(dir)) {
 
 let app = express()
 
-// console.log(process.argv)
-// console.log(args)
-// console.log(dir)
-
 app.use('/', express.static(dir), serveIndex(dir, {'icons': true}))
 
 app.listen(port, () => {
 
-	console.log(`serving ${dir} over http://localhost:${port}`)
+	let localhost = `http://localhost:${port}`
+
+	console.log('quick-static:')
+	console.log(`    serving ${chalk.blue(dir)}`)
+	console.log(`    over ${chalk.red(localhost)}`)
+
+}).on('error', e => {
+
+	if (e.code === 'EADDRINUSE') {
+
+		console.log(chalk.red(`oups! the port ${port} is already in use!`))
+
+	} else {
+
+		console.log(e)
+
+	}
 
 })
 
